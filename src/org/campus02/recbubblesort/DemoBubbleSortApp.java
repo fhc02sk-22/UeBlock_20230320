0febb492b8024ac37ae8050e5d12f9ba6b83389d
skochauf <stephan.kochauf@lv.campus02.at>
2023-03-19 19:56:04 +0100

    Adding rec Example

---
+++ src/org/campus02/recbubblesort/DemoBubbleSortApp.java
@@ -0,0 +1,28 @@
+package org.campus02.recbubblesort;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class DemoBubbleSortApp {
+    public static void main(String[] args) {
+        int size = 5;
+        int[] elements = new int[size];
+
+        Random rand = new Random();
+        // Generates a random value
+        System.out.println("rand.nextInt(100) = " + rand.nextInt(100));
+
+        // Init Array with random values
+
+        // Sort Array
+
+        System.out.println(Arrays.toString(elements));
+
+    }
+
+    public static void sort(int[] elements) {
+
+    }
+
+
+}
---
+++ src/org/campus02/reccounter/DemoCounterApp.java
@@ -0,0 +1,14 @@
+package org.campus02.reccounter;
+
+public class DemoCounterApp {
+    public static void main(String[] args) {
+
+        System.out.println("count(5) = " + count(5)); // should return 1, 2, 3, 4, 5
+
+    }
+
+    public static String count(int end) {
+
+        return "";
+    }
+}
